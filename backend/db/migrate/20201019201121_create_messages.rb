7a70341978bc45b28e62a77cb3076a3148c0a8dd
jlonetree <jlonetree@gmail.com>
2020-10-23 03:09:58 -0500

    minor change

--- backend/db/migrate/20201019201121_create_messages.rb
+++ backend/db/migrate/20201019201121_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t. :message
+      t.string :message
       t.integer :username_id
       t.integer :message_board_id
 
